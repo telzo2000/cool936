bad3d2e729e55325ba32b6b980fa8c2295ba053d
木村 光輝 <67196597+telzo2000@users.noreply.github.com>
2024-01-06 10:05:35 +0900

    fix

--- firmware/qmk0.22/qmk_firmware_cool936/cool936/keymaps/vial/config.h
+++ firmware/qmk0.22/qmk_firmware_cool936/cool936/keymaps/vial/config.h
@@ -2,3 +2,5 @@
 
 #pragma once
 #define VIAL_KEYBOARD_UID {0x4B, 0xCA, 0xEC, 0xFB, 0x2E, 0x7D, 0x33, 0x73}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
--- firmware/qmk0.22/qmk_firmware_cool936/cool936/rules.mk
+++ firmware/qmk0.22/qmk_firmware_cool936/cool936/rules.mk
@@ -1,2 +1,33 @@
-# This file intentionally left blank
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+# Build Options
+#   change yes to no to disable
+#
+BOOTMAGIC_ENABLE = yes    # Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no      # Mouse keys
+EXTRAKEY_ENABLE = no       # Audio control and System control
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+# Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
+SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
+# if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = no            # USB Nkey Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+BLUETOOTH_ENABLE = no       # Enable Bluetooth
+AUDIO_ENABLE = no           # Audio output
 SPLIT_KEYBOARD = yes
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+COMMAND_ENABLE = no
+MAGIC_ENABLE = no
+CONSOLE_ENABLE = no
+MUSIC_ENABLE = no
+
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE = yes
+KEY_OVERRIDE_ENABLE = no
